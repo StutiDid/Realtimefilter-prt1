14a91392e73c6943392e7ee71c2ca245235af90b
StutiDid <77789710+StutiDid@users.noreply.github.com>
2021-10-19 14:35:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function proload(){
+}
+
+function setup (){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+ function draw(){
+ }
+ function take_snapshot(){
+     save ('myFilterImage.png');
+ }
+ 
